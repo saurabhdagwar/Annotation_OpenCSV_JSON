bc8c99f198441dd8a9e7febe080ce5e5856da23a
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-25 12:42:44 +0530

    [Saurabh]WIP: POJO class and main method

---
+++ src/main/java/com/Bridgelabz/CSVUser.java
@@ -0,0 +1,24 @@
+package com.Bridgelabz;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVUser {
+    @CsvBindByName
+    private String name;
+    @CsvBindByName(column = "email", required = true)
+    private String email;
+    @CsvBindByName(column = "phone")
+    private String phoneNo;
+    @CsvBindByName
+    private String country;
+
+    @Override
+    public String  toString() {
+        return "CSVUser :{ " +
+                "name = " + name + '\'' +
+                ", Email = " + email + '\'' +
+                ", Phone = " + phoneNo + '\'' +
+                ", Country = " + country + '\'' +
+    '}';
+    }
+}
